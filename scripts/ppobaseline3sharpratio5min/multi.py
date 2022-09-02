3ba72be97cd5c3e3b5ff1c07a1df671bbd06a88d
yamaguchi-milkcocholate <zuuuubo.tetsu@outlook.jp>
2022-09-02 22:30:45 +0900

    [update] CV全てを回す処理

---
+++ scripts/ppobaseline3sharpratio5min/multi.py
@@ -0,0 +1,80 @@
+import sys
+import json
+import torch
+from pathlib import Path
+from typing import *
+
+rootdir = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(rootdir))
+
+import src.models.ppobaseline3 as models
+import src.markets.ppobaseline3sharpratio as markets
+import src.utils.data as data
+import src.utils.plot as plot
+import src.utils.seed as seed
+
+
+def main(fold: int):
+    seed.seed_everything(seed=42)
+    exptdir = Path(__file__).resolve().parent
+    savedir = exptdir / "out" / f"fold{fold}"
+    savedir.mkdir(exist_ok=True, parents=True)
+
+    config = json.load(open(exptdir / "config.json", "r"))
+    ppo_params = config["ppo_params"]
+    train_params = config["train_params"]
+    action_params = config["action_params"]
+
+    df, features = data.load_bybit_data(
+        num_devide=train_params["NUM_DEVIDE"],
+        lags=train_params["LAGS"],
+        interval=train_params["MINUTES"],
+    )
+
+    df_train, df_eval = (
+        df.loc[df["fold"] != (fold - 1)].reset_index(drop=True),
+        df.loc[df["fold"] == (fold - 1)].reset_index(drop=True),
+    )
+
+    train_env = markets.MarketEnv(
+        df=df_train,
+        features=features,
+        num_steps=train_params["NUM_TAIN_ENV_STEPS"],
+        action_params=action_params,
+        n_lag=train_params["N_LAG"],
+        market_cls=markets.Market,
+        is_single_transaction=True,
+    )
+    eval_env = markets.MarketEnv(
+        df=df_eval,
+        features=features,
+        num_steps=None,
+        action_params=action_params,
+        n_lag=train_params["N_LAG"],
+        market_cls=markets.Market,
+        is_single_transaction=False,
+    )
+    model = models.create_model(
+        savedir=savedir / "log", env=train_env, ppo_params=ppo_params
+    )
+    model.learn(
+        total_timesteps=train_params["NUM_STEPS"],
+        eval_env=eval_env,
+        eval_freq=train_params["EVAL_FREQ"],
+    )
+    model.save(savedir / "model")
+
+    plot.plot_from_baseline3(fromdir=savedir / "log", todir=savedir)
+
+    eval_market = markets.Market(
+        df=df_eval,
+        features=features,
+        action_params=action_params,
+        n_lag=train_params["N_LAG"],
+        is_single_transaction=False,
+    )
+    plot.eval_and_plot(model=model, market=eval_market, savedir=savedir)
+
+
+if __name__ == "__main__":
+    main(fold=int(sys.argv[1]))
